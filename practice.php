1615ab96d7927a182991c81345fd045f1c592bda
ko-ankoromoti <naha.ameku.1965zai@gmeil.com>
2022-04-19 05:29:56 +0000

    Laravel03課題

--- practice.php
+++ practice.php
@@ -39,3 +39,59 @@ $calendar_2018 = [
 
 echo $calendar_2018["December"];
 echo "\n";
+
+
+
+
+$name = "Anzai";
+if ($name >= "Anzai") {
+  echo "あなたの名前ではありません";
+} else {
+  echo "私は あなたの名前 です。";
+}
+echo "\n";
+
+
+$name = "Anzai";
+switch ($name) {
+  case "Anzai";
+   echo "私は あなたの名前 です。";
+   break;
+  case "Yamada";
+   echo "あなたの名前ではありません";
+   break;
+}
+echo "\n";
+
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 10000; $i++) {
+  $total += $i;
+}
+echo $total;
+echo "\n";
+
+$fruits = array("strawbrry", "banana", "painappuru", "kiui", "orange");
+echo count($fruits);
+for ($i = 0; $i < count($fruits);$i++) {
+  echo "要素は" . $fruits[$i];
+  echo "\n";
+}
+ echo "\n";
+ 
+ 
+ /* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for ($i = $start; $i < $end; $i++){
+
+ // 5で割り切れたら{}内を実行する
+  if($i % 5 === 0){
+  echo $i;
+  }
+}
+echo "\n";
+
+
